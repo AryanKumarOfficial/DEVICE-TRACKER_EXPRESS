e19421042894e507893d656fd9a0bd1c2c61ddfa
Aryan Kumar <arynak9163@gmail.com>
2024-08-07 14:43:08 +0530

    chore: Update socket variable name, add connection event listener, and improve geolocation handling

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
 const express = require("express");
-const socket = require("socket.io");
+const socketIO = require("socket.io");
 const http = require("http");
 const path = require("path");
+const os = require("os");
+
 const app = express();
-
 const server = http.createServer(app);
+const io = socketIO(server);
 
-const io = socket(server);
-io.on("connection", function (socket) {
-    console.log("connected");
-})
+const port = 3000;
 
+// Function to get the IPv4 address
+const getIPv4Address = () => {
+    const interfaces = os.networkInterfaces();
+    for (let interfaceName in interfaces) {
+        for (let iface of interfaces[interfaceName]) {
+            if (iface.family === 'IPv4' && !iface.internal) {
+                return iface.address;
+            }
+        }
+    }
+    return '127.0.0.1';
+};
+
+// Middleware
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", function (req, res) {
+// Routes
+app.get("/", (req, res) => {
     res.render("index");
-})
+});
 
-server.listen(3000);
+// Socket.io connection
+io.on("connection", (socket) => {
+    socket.on("send-location", (data) => {
+        io.emit("receive-location", { id: socket.id, ...data });
+    });
+
+    socket.on("disconnect", () => {
+        io.emit("user-disconnected", socket.id);
+    });
+});
+
+// Start server
+server.listen(port, () => {
+    const IPv4 = getIPv4Address();
+    console.log(`Server is listening on:\n http://${IPv4}:${port}\n http://localhost:${port}`);
+});
